94428ccc66c217fc48032d847dffb1dddbce999a
weechengkhor <chengkhor798@gmail.com>
2019-12-02 13:51:57 +0800

    upload axios files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//axios api
+import axios from 'axios';
+
+class App extends Component {
+
+  state = {
+    posts: []
+  }
+  componentDidMount() {
+    console.log('did mount');
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then (res => {
+      console.log('res', res) 
+      this.setState({
+        posts: res.data
+      })
+    })
+
+  }
+
+  
+  render() {
+    const {posts} = this.state;
+    const postsList = posts.length ? (posts.map( (item) => 
+      <div className="post card" key={item.id}>
+        <div className="card-content">
+          <span className="card-title"> {item.title}</span>
+          <p>{item.body}</p>
+        </div>
+       </div>
+     )) : (<p>No data</p>)
+    return (
+      <div className="container">
+        <h3>Axios Example</h3>
+        {postsList}
+      </div>
+    )
+
+  }
 }
 
-export default App;
+export default App
